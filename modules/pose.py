736fe340677afa73f8c6d814fc68efc2d12fc54f
HSIEH YU JEN <39560035+a5372935@users.noreply.github.com>
2020-12-26 18:58:01 +0800

    Add files via upload
    Optimized performance & catch some bugs

--- modules/pose.py
+++ modules/pose.py
@@ -4,7 +4,7 @@ import random
 from modules.keypoints import BODY_PARTS_KPT_IDS, BODY_PARTS_PAF_IDS
 from modules.one_euro_filter import OneEuroFilter
 import time
-from yolo import YOLO
+# from yolo import YOLO
 
 class Pose:
     num_kpts = 18
@@ -47,8 +47,7 @@ class Pose:
             self.id = Pose.last_id + 1
             Pose.last_id += 1
 
-    def draw(self, img, img_roi_coordinate):
-        # yolo = YOLO()
+    def draw(self, img, img_roi_coordinate = None):
         assert self.keypoints.shape == (Pose.num_kpts, 2)
 
         for part_id in range(len(BODY_PARTS_PAF_IDS) - 2):
@@ -56,15 +55,15 @@ class Pose:
             global_kpt_a_id = self.keypoints[kpt_a_id, 0]
             if global_kpt_a_id != -1:
                 x_a, y_a = self.keypoints[kpt_a_id]
-                x_a = x_a + img_roi_coordinate[0]
-                y_a = y_a + img_roi_coordinate[2]
+                # x_a = x_a + img_roi_coordinate[0]
+                # y_a = y_a + img_roi_coordinate[2]
                 cv2.circle(img, (int(x_a), int(y_a)), 5, Pose.color, -1)
             kpt_b_id = BODY_PARTS_KPT_IDS[part_id][1]
             global_kpt_b_id = self.keypoints[kpt_b_id, 0]
             if global_kpt_b_id != -1:
                 x_b, y_b = self.keypoints[kpt_b_id]
-                x_b = x_b + img_roi_coordinate[0]
-                y_b = y_b + img_roi_coordinate[2]
+                # x_b = x_b + img_roi_coordinate[0]
+                # y_b = y_b + img_roi_coordinate[2]
                 # print("kpt_b_id = " + str(Pose.kpt_names[kpt_b_id]) + "; " + str(int(x_b)) + ", " + str(int(y_b)))
                 cv2.circle(img, (int(x_b), int(y_b)), 5, Pose.color, -1)
                 cv2.putText(img, str(Pose.kpt_names[kpt_b_id]) + " : " + str(int(x_b)) + ", " + str(int(y_b)), 
@@ -73,24 +72,50 @@ class Pose:
                 cv2.line(img, (int(x_a), int(y_a)), (int(x_b), int(y_b)), (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), 3, cv2.LINE_4)
 
 
-def get_similarity(a, b, threshold=0.5):
+def get_similarity(a, b, threshold = 0.5):
     num_similar_kpt = 0
     for kpt_id in range(Pose.num_kpts):
         if a.keypoints[kpt_id, 0] != -1 and b.keypoints[kpt_id, 0] != -1:
+            
             distance = np.sum((a.keypoints[kpt_id] - b.keypoints[kpt_id]) ** 2)
+            # print(Pose.kpt_names[kpt_id] + " : " + str(distance))
+            # print(distance)
             area = max(a.bbox[2] * a.bbox[3], b.bbox[2] * b.bbox[3])
             similarity = np.exp(-distance / (2 * (area + np.spacing(1)) * Pose.vars[kpt_id]))
             if similarity > threshold:
                 num_similar_kpt += 1
+       
     return num_similar_kpt
 
-def get_distance(a, b):
-    x = a[0] - b[0]
-    y = a[1] - b[1]
-    point_distance = np.sqrt(x ** 2 + y ** 2)
-    return point_distance
+def get_distance(current_poses, previous_poses):
+    point_distance_id = []
+    # print(current_poses[0].keypoints[1, 0])
+    # print(len(current_poses))
+    for i in range(len(current_poses)):
+        try:
+            # print("current_poses = " + str(current_poses[i].keypoints[1, 1]))
+            # print("previous_poses = " + str(previous_poses[i].keypoints[1, 1]))
+            # print("current_poses - previous_poses = " + str(current_poses[i].keypoints[1, 0] - previous_poses[i].keypoints[1, 0]))
+            # print("current_poses = " + str(current_poses[0].keypoints[1, 0]))
+            # print("previous_poses = " + str(previous_poses[i].keypoints[1, 0]))
+            if current_poses[i].keypoints[1, 0] != -1 and previous_poses[i].keypoints[1, 0] != -1:
+                
+                # calculate variation of two-point
+                # x = current_poses[i].keypoints[1][0] - previous_poses[i].keypoints[1][0]
+                # y = current_poses[i].keypoints[1][1] - previous_poses[i].keypoints[1][1]
+                # point_distance = np.sqrt(x ** 2 + y ** 2)
 
-def track_poses(previous_poses, current_poses, threshold=3, smooth=False):
+                # calculate variation of y-axis
+                point_distance = previous_poses[i].keypoints[1][1] - current_poses[i].keypoints[1][1]
+                point_distance_id.append(point_distance)
+            else:
+                point_distance_id.append(0)
+        except Exception as e:
+            pass
+        continue
+    return point_distance_id
+
+def track_poses(previous_poses, current_poses, threshold = 3, smooth=False):
     """Propagate poses ids from previous frame results. Id is propagated,
     if there are at least `threshold` similar keypoints between pose from previous frame and current.
     If correspondence between pose on previous and current frame was established, pose keypoints are smoothed.
@@ -101,9 +126,17 @@ def track_poses(previous_poses, current_poses, threshold=3, smooth=False):
     :param smooth: smooth pose keypoints between frames
     :return: None
     """
-    current_poses = sorted(current_poses, key=lambda pose: pose.confidence, reverse=True)  # match confident poses first
+    # print("")
+    # print("confidence_before")
+    # for i in current_poses:
+    #     print(i.confidence)
+    current_poses = sorted(current_poses, key = lambda pose: pose.confidence, reverse=True)  # match confident poses first
+    # print("confidence_after")
+    # for i in current_poses:
+    #     print(i.confidence)
     mask = np.ones(len(previous_poses), dtype=np.int32)
     for current_pose in current_poses:
+        # print(current_pose.confidence)
         best_matched_id = None
         best_matched_pose_id = None
         best_matched_iou = 0
@@ -134,10 +167,13 @@ def track_poses(previous_poses, current_poses, threshold=3, smooth=False):
                 current_pose.keypoints[kpt_id, 0] = current_pose.filters[kpt_id][0](current_pose.keypoints[kpt_id, 0])
                 current_pose.keypoints[kpt_id, 1] = current_pose.filters[kpt_id][1](current_pose.keypoints[kpt_id, 1])
             current_pose.bbox = Pose.get_bbox(current_pose.keypoints)
+        
+        # print("current_poses : " + str(len(current_poses)))
+        # print("previous_poses : " + str(len(previous_poses)))
     
         if len(previous_poses) != 0: # 當前一個poses位置不為0時
-            Current_neck = current_poses[0].keypoints[1]  # 紀錄neck位置
-            Previous_neck = previous_poses[0].keypoints[1]
-            Point_dis = get_distance(Current_neck, Previous_neck) # 計算 neck 差值
-
+            # Current_neck = current_poses[0].keypoints[1]  # 紀錄neck位置
+            # Previous_neck = previous_poses[0].keypoints[1]
+            Point_dis = get_distance(current_poses, previous_poses) # 計算 neck 差值
+            
             return Point_dis
